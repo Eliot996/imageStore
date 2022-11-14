88c350b87aa8f02bc637b745de6f13dfd7a89c53
Eliot996 <math10t6@stud.kea.dk>
2022-11-14 14:55:39 +0100

    added delete

--- src/main/java/com/example/imagestorebackend/controllers/MainController.java
+++ src/main/java/com/example/imagestorebackend/controllers/MainController.java
@@ -20,8 +20,24 @@ public class MainController {
     }
 
     @PostMapping
-    public ResponseEntity<Image> app(@RequestBody Image image){
+    public ResponseEntity<Image> add(@RequestBody Image image){
+        image.setId((int) Math.floor(Math.random() * 10000));
         list.add(image);
         return ResponseEntity.ok().body(image);
+    }
+
+    @DeleteMapping("/{image}")
+    public ResponseEntity<Image> delete(@PathVariable int image) {
+        boolean foundImage = false;
+        Image found = new Image();
+
+        for (int i = 0; i < list.size(); i++) {
+            found = list.get(i);
+            if (found.getId() == image) {
+                list.remove(found);
+                foundImage = true;
+            }
+        }
+        return foundImage ? ResponseEntity.ok().body(found) : ResponseEntity.badRequest().body(null);
     }
 }
